7ae428929b4ab2955aaea5e51ab3090316d6f3b0
unknown <majdeddine.matoussi@gmail.com>
2021-07-19 14:56:33 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const options = {
+  bottom: '15px', // default: '32px'
+  right: '15px', // default: '32px'
+  left: 'unset', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#504f57', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: false // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
